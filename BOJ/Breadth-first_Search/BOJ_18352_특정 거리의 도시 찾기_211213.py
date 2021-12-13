cea297227fb94d8a45010fa98179b70b4a106ce7
WonChul <wcshin1201@gmail.com>
2021-12-13 20:07:01 +0900

    upload BOJ 20211213

--- BOJ/Breadth-first_Search/BOJ_18352_특정 거리의 도시 찾기_211213.py
+++ BOJ/Breadth-first_Search/BOJ_18352_특정 거리의 도시 찾기_211213.py
@@ -4,23 +4,25 @@ graph = [[] for _ in range(n+1) ]
 for _ in range(m):
     s, e = list(map(int, input().split()))
     graph[s].append(e)
-visited = [0]* (n+1)
 
+visited = [False]*(n+1)
+distance = [0]* (n+1)
+
+result = []
 def bfs(start):
     q = deque([start])
+    visited[start] = True
     while q:
         n = q.popleft()
         for node in graph[n]:
-            if visited[node] == 0:
-                visited[node] = visited[n]+1
+            if not visited[node]:
+                visited[node] = True
+                distance[node] = distance[n]+1
                 q.append(node)
-                
+                if distance[node] == k:
+                    result.append(node)
 bfs(x)
-result = []
-for i, v in enumerate(visited):
-    if v == k:
-        result.append(i)
-        
+result.sort()        
 if len(result) == 0:
     print(-1)
 else:
