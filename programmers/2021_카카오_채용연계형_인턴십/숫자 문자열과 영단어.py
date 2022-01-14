6584bd1987a8e39569be1274b6141d604526ac20
WonChul <wcshin1201@gmail.com>
2022-01-14 14:03:33 +0900

    upload programmers 숫자 문자열과 영단어

---
+++ programmers/2021_카카오_채용연계형_인턴십/숫자 문자열과 영단어.py
@@ -0,0 +1,20 @@
+import re
+
+def solution(s):
+    dictionary = {
+                'zero' : '0',
+                'one' : '1',
+                'two' : '2',
+                'three' : '3',
+                'four' : '4',
+                'five' : '5',
+                'six' : '6',
+                'seven' : '7',
+                'eight' : '8',
+                'nine' : '9',
+             }
+    
+    for key in dictionary.keys():
+        p = re.compile(key)
+        s = p.sub(dictionary[key],s)
+    return int(s)
