25afa70bae4a7b53197a0dd5d990551f96cf5b6b
WonChul <wcshin1201@gmail.com>
2022-01-21 17:20:05 +0900

    upload programmers 징검다리 건너기

---
+++ programmers/2019_카카오_개발자_겨울_인턴십/징검다리 건너기.py
@@ -0,0 +1,23 @@
+def solution(stones, k):
+    start = 0
+    end = max(stones)
+    while start <= end:
+        mid = int((start + end) / 2) 
+        arr = list(map(lambda x : x-mid, stones))
+        count = 0
+        for i in arr:
+            if count < k:
+                if i <= 0:
+                    count += 1
+                else:
+                    count = 0
+            else:
+                break
+
+        if count < k:
+            start = mid + 1
+        else:
+            end = mid - 1
+            result = mid
+
+    return result
