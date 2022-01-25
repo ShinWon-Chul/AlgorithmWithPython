66763d5c200a2f5433eee08835058cae056fd2f7
WonChul <wcshin1201@gmail.com>
2022-01-25 20:36:19 +0900

    update programmers 호텔 방 배정

--- programmers/2019_카카오_개발자_겨울_인턴십/호텔 방 배정.py
+++ programmers/2019_카카오_개발자_겨울_인턴십/호텔 방 배정.py
@@ -5,7 +5,7 @@ sys.setrecursionlimit(10**6)
 def find_room(room_dict, room):
     if room_dict[room] == 0:
         room_dict[room] = room + 1
-        return room_dict[room] -1
+        return room
     elif room_dict[room] != 0:
         room_dict[room] = find_room(room_dict, room_dict[room])
         return room_dict[room]
