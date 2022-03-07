6e57bdae05aef010e4bde483adc8b6c10119a7e2
ShinWon-Chul <wcshin1201@gmail.com>
2022-03-08 01:24:08 +0900

    update code

--- note/Probability Theory/combination.py
+++ note/Probability Theory/combination.py
@@ -3,3 +3,11 @@ import itertools
 data = [1, 2, 3]
 for x in itertools.combinations(data, 2):
     print(list(x))
+
+"""
+nCr
+n!/(n-r)!r!
+(1, 2)
+(1, 3)
+(2, 3)
+"""
--- note/Probability Theory/permutation.py
+++ note/Probability Theory/permutation.py
@@ -1,5 +1,16 @@
 import itertools
 
-data = [1, 2]
+data = [1, 2, 3]
 for x in itertools.permutations(data, 2):
     print(list(x))
+
+"""
+nPr
+n!/(n-r)!
+(1, 2)
+(1, 3)
+(2, 1)
+(2, 3)
+(3, 1)
+(3, 2)
+"""
