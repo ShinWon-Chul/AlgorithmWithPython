49c4b5e72ea584163da98b66c397d99f3cddec0f
ShinWon-Chul <wcshin1201@gmail.com>
2022-03-09 17:26:35 +0900

    update code

--- programmers/2022_KAKAO_BLIND_RECRUITMENT/파괴되지 않은 건물.py
+++ programmers/2022_KAKAO_BLIND_RECRUITMENT/파괴되지 않은 건물.py
@@ -34,25 +34,3 @@ def solution(board, skill):
             if col >0:
                 cnt += 1
     return cnt
-
-#더 빠르고 짧은 코드 풀이
-from collections import defaultdict
-
-def solution(gems):
-    min_gems  = int(1e9)
-    len_gems = len(gems) 
-    n_gems = len(set(gems))
-    end = 0
-    temp = defaultdict(lambda : 0)
-    for start, gem in enumerate(gems):
-        while len(temp) < n_gems and end < len_gems: 
-            temp[gems[end]] += 1
-            end += 1
-        if len(temp) == n_gems:
-            if min_gems > end-start:
-                min_gems = end-start
-                result = [start+1, end]      
-        temp[gem] -= 1
-        if temp[gem] == 0:
-            del(temp[gem])
-    return result
