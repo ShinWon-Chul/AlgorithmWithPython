16180a7f7873b6ac1a4736eafd8808e2bd01ad82
ShinWon-Chul <wcshin1201@gmail.com>
2022-03-22 22:37:46 +0900

    upload programmers 단속카메라(greedy).py

---
+++ programmers/연습문제/level3/단속카메라(greedy).py
@@ -0,0 +1,14 @@
+def solution(routes):
+    routes.sort()
+    cam = -30000
+    count = 0
+    for start, end in routes:
+        if start > cam:
+            count += 1
+            cam = end
+        elif start < cam:
+            if end < cam:
+                cam = end
+        else:
+            continue
+    return count
