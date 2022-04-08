e42577a7b3ee301e1272d50130e99d981d9f1553
ShinWon-Chul <wcshin1201@gmail.com>
2022-04-08 13:47:07 +0900

    update code

--- programmers/연습문제/level4/징검다리(binarysearch).py
+++ programmers/연습문제/level4/징검다리(binarysearch).py
@@ -1,3 +1,4 @@
+import copy
 def solution(distance, rocks, n):
     rocks += [0, distance]
     rocks.sort()
@@ -7,17 +8,14 @@ def solution(distance, rocks, n):
     
     while start<=end:
         mid = int((start+end)/2)
-        
         count = 0
         remain = 0
         dist = 0
+        
         for i in range(1, n_rocks):
             dist = abs(rocks[remain]-rocks[i])
             if dist < mid:
-                if i == n_rocks-1:
-                    count += 1
-                else:
-                    count += 1
+                count += 1
             else:
                 dist = 0
                 remain = i
@@ -28,5 +26,5 @@ def solution(distance, rocks, n):
         else:
             start = mid + 1
             result = mid
-    
+            
     return result
