d2001062f0398fbb1491179a5e7b6f8d0c8f5438
ShinWon-Chul <wcshin1201@gmail.com>
2022-04-17 16:14:13 +0900

    upload 예상 대진표.py

---
+++ programmers/연습문제/level2/예상 대진표.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(n,a,b):
+    answer = 1 
+    while True:
+        if math.ceil(a/2) == math.ceil(b/2):
+            break
+        answer += 1
+        a = math.ceil(a/2)
+        b = math.ceil(b/2)
+    return answer
