f50c4ad2eb067b49aef6500d1d11ce6d7ab7c9ba
ShinWon-Chul <wcshin1201@gmail.com>
2022-04-24 13:54:28 +0900

    upload programmers 주식가격(stack, queue).py

---
+++ programmers/연습문제/level2/주식가격(stack, queue).py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    q = deque(prices)
+    while q:
+        price = q.popleft()
+        count = 0
+        if len(q) != 0:
+            for p in q:
+                count += 1
+                if price > p:
+                    break
+        answer.append(count)
+
+    return answer
