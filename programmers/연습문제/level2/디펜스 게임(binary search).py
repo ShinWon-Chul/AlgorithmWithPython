4ef25fdf04e9652f0472c0ea2059784b60c04508
shinwonchul <wcshin1201@gmail.com>
2023-05-02 16:29:31 +0900

    디펜스 게임(binary search).py

--- programmers/연습문제/level2/디펜스 게임(binary search).py
+++ programmers/연습문제/level2/디펜스 게임(binary search).py
@@ -11,9 +11,13 @@ def solution(n, k, enemy):
         temp_enemy = temp_enemy[k:]
         nenemy = sum(temp_enemy)
 
+        # 내가 상대할 수 있는 적수보다 적수가 작거나 같다면 라운드를 증가
+        # answer가 위로 와야되는 이유는 찾아야 되는 지점이 큰지점이 아니라 작거나 같아야 하므로
+        # 적수가 더 많다면 답이 될 수 없기 때문
         if nenemy <= n:
             start = mid + 1
-            answer = mid
+            answer = mid # 
+            
         else:
             end = mid - 1
 
