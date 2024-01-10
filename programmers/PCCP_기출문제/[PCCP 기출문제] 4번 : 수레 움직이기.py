a662aa055eb06db003b75549424d7469f9e04acd
shinwonchul <wcshin1201@gmail.com>
2024-01-10 11:31:05 +0900

    solve

--- programmers/PCCP_기출문제/[PCCP 기출문제] 4번 : 수레 움직이기.py
+++ programmers/PCCP_기출문제/[PCCP 기출문제] 4번 : 수레 움직이기.py
@@ -1,88 +1,144 @@
 import sys
+import copy
 sys.setrecursionlimit(10**7)
 # turn을 저장할 배열
-turn_arr = []
+answer = int(1e9)
 # 상하좌우
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-def dfs(maze, turn, r_loc, b_loc, r_arrive, b_arrive, width, high, rvisited, bvisited):
-    global tunr_arr
+def dfs(maze, turn, r_loc, b_loc, r_arrive, b_arrive, width, height, rvisited, bvisited):
+    global answer
     # 두 수레가 모두 도착한 경우
     if r_loc == r_arrive and b_loc == b_arrive:
-        turn_arr.append(turn)
+        answer = min(answer, turn)
         return
-    
+
+    # 턴이 이미 도착한 최솟값을 넘어가면 더이상 탐색 할 필요가 없다.
+    if turn >= answer:    
+        return
     #현재 위치
     rx, ry = r_loc
     bx, by = b_loc
+
+
     for i in range(4):
-        # 빨강 먼저 업데이트 
-        nrx = rx + dx[i]
-        nry = ry + dy[i]
-        # 빨강은 파랑의 현재 위치, 벽, 이전에 지나온 길을 갈 수 없음
-        if nrx != bx or nry != by and maze[nry][nrx] != 5 and rvisited[nry][nrx] != True: 
+        # 빨강이 도착해 있는 경우
+        if rx == r_arrive[0] and ry == r_arrive[1]: 
+            # 파랑만 업데이트
             nbx = bx + dx[i]
             nby = by + dy[i]
-            # 파랑은 업데이트 된 빨강 위치, 벽, 이전에 지나온 길을 갈 수 없음
-            if nbx != nrx or nry != nby and maze[nby][nbx] != 5 and bvisited[nry][nrx] != True:
-                # 범위를 벗어나는지 판단
-                if (0 <= nrx < width and 0 <= nry < high) and (0 <= nbx < width and 0 <= nby < high):
+            # 범위를 벗어나는지 판단, 빨강 위치에 이동할 수 없음
+            if (0 <= nbx < width and 0 <= nby < height) and (nbx != rx or ry != nby):
+                # 벽, 이전에 지나온 길을 갈 수 없음
+                if maze[nby][nbx] != 5 and bvisited[nby][nbx] != True:
                     # 위 조건을 모두 만족했다면 만족했다면 
-                    # r_loc, b_loc, r_arrive, b_arrive업데이트
-                    nr_loc = [nrx, nry]
+                    # r_arrive, b_arrive업데이트
+                    # nr_loc = [nrx, nry]
                     nb_loc = [nbx, nby]
-                    rvisited[ry][rx] = True
-                    bvisited[ry][rx] = True
-                    dfs(maze, turn + 1, nr_loc, nb_loc, r_arrive, b_arrive, width, high, rvisited, bvisited)
-        
-        # backtracking
-        rvisited[ry][rx] = False
-        bvisited[ry][rx] = False
-
-        ## 동일한 로직으로 파랑 업데이트
-        # 파랑 먼저 업데이트 
-        nbx = bx + dx[i]
-        nby = by + dy[i]
-        # 파랑은 빨강의 현재 위치, 벽, 이전에 지나온 길을 갈 수 없음
-        if nbx != rx or nby != ry and maze[nby][nbx] != 5 and bvisited[nby][nbx] != True: 
+                    # n_bvisited = copy.deepcopy(bvisited)
+                    bvisited[nby][nbx] = True
+                    dfs(maze, turn + 1, r_loc, nb_loc, r_arrive, b_arrive, width, height, rvisited, bvisited)
+                    bvisited[nby][nbx] = False
+        else:
+            # 빨강 먼저 업데이트 
             nrx = rx + dx[i]
             nry = ry + dy[i]
-            # 빨강은 업데이트 된 파랑 위치, 벽, 이전에 지나온 길을 갈 수 없음
-            if nrx != nbx or nry != nby and maze[nry][nrx] != 5 and rvisited[nry][nrx] != True:
-                # 범위를 벗어나는지 판단
-                if (0 <= nrx < width and 0 <= nry < high) and (0 <= nbx < width and 0 <= nby < high):
+            # 범위를 벗어나는지 판단, 파랑 위치에 이동할 수 없음
+            if (0 <= nrx < width and 0 <= nry < height) and (nrx != bx or nry != by):
+                # 벽, 이전에 지나온 길을 갈 수 없음
+                if maze[nry][nrx] != 5 and rvisited[nry][nrx] != True:
+                    for j in range(4):
+                        nbx = bx + dx[j]
+                        nby = by + dy[j]
+                        # 범위를 벗어나는지 판단, 빨강 위치에 이동할 수 없음
+                        if (0 <= nbx < width and 0 <= nby < height) and (nbx != nrx or nry != nby):
+                            # 벽, 이전에 지나온 길을 갈 수 없음
+                            if maze[nby][nbx] != 5 and bvisited[nby][nbx] != True:
+                                # 위 조건을 모두 만족했다면 만족했다면 
+                                # r_arrive, b_arrive업데이트
+                                nr_loc = [nrx, nry]
+                                nb_loc = [nbx, nby]
+                                # n_rvisited = copy.deepcopy(rvisited)
+                                # n_bvisited = copy.deepcopy(bvisited)
+                                rvisited[nry][nrx] = True
+                                bvisited[nby][nbx] = True
+                                dfs(maze, turn + 1, nr_loc, nb_loc, r_arrive, b_arrive, width, height, rvisited, bvisited)
+                                # backtracking
+                                rvisited[nry][nrx] = False
+                                bvisited[nby][nbx] = False
+            
+
+
+        ## 동일한 로직으로 파랑 업데이트
+        # 파랑이 도착해 있는 경우
+        if bx == b_arrive[0] and by == b_arrive[1]: 
+            # 빨강만 업데이트
+            nrx = rx + dx[i]
+            nry = ry + dy[i]
+            # 범위를 벗어나는지 판단, 빨강 위치에 이동할 수 없음
+            if (0 <= nrx < width and 0 <= nry < height) and (nrx != bx or nry != by):
+                # 벽, 이전에 지나온 길을 갈 수 없음
+                if maze[nry][nrx] != 5 and rvisited[nry][nrx] != True:
                     # 위 조건을 모두 만족했다면 만족했다면 
-                    # r_loc, b_loc, r_arrive, b_arrive업데이트
+                    # r_arrive, b_arrive업데이트
+                    # nr_loc = [nrx, nry]
                     nr_loc = [nrx, nry]
-                    nb_loc = [nbx, nby]
-                    rvisited[ry][rx] = True
-                    bvisited[ry][rx] = True
-                    dfs(maze, turn + 1, nr_loc, nb_loc, r_arrive, b_arrive, width, high, rvisited, bvisited)
-        
-        # 더이상 진행할 수 없는 경우
-            # 배열에 0을 추가하고
-            turn_arr.append(0)
-            # dfs 생략
-            continue
+                    # n_rvisited = copy.deepcopy(rvisited)
+                    rvisited[nry][nrx] = True
+                    dfs(maze, turn + 1, nr_loc, b_loc, r_arrive, b_arrive, width, height, rvisited, bvisited)
+                    rvisited[nry][nrx] = False
+        else:
+            # 파랑 먼저 이동
+            nbx = bx + dx[i]
+            nby = by + dy[i]
+            # 범위를 벗어나는지 판단, 빨강 위치에 이동할 수 없음
+            if (0 <= nbx < width and 0 <= nby < height) and (nbx != rx or nby != ry):
+                # 벽, 이전에 지나온 길을 갈 수 없음
+                if maze[nby][nbx] != 5 and bvisited[nby][nbx] != True: 
+                    for j in range(4):
+                        nrx = rx + dx[j]
+                        nry = ry + dy[j]
+                        # 범위를 벗어나는지 판단, 파 위치에 이동할 수 없음
+                        if (0 <= nrx < width and 0 <= nry < height) and (nrx != nbx or nry != nby):
+                            # 벽, 이전에 지나온 길을 갈 수 없음
+                            if  maze[nry][nrx] != 5 and rvisited[nry][nrx] != True:
+                                # 위 조건을 모두 만족했다면 만족했다면 
+                                # r_arrive, b_arrive업데이트
+                                nr_loc = [nrx, nry]
+                                nb_loc = [nbx, nby]
+                                # n_rvisited = copy.deepcopy(rvisited)
+                                # n_bvisited = copy.deepcopy(bvisited)
+                                rvisited[nry][nrx] = True
+                                bvisited[nby][nbx] = True
+                                dfs(maze, turn + 1, nr_loc, nb_loc, r_arrive, b_arrive, width, height, rvisited, bvisited)
+                                # backtracking
+                                rvisited[nry][nrx] = False
+                                bvisited[nby][nbx] = False
             
 def solution(maze):
-    answer = 0
     init_turn = 0
-    high = len(maze)
+    height = len(maze)
     width = len(maze[0])
-    rvisited = [[False for _ in range(width)] for _ in range(high)]
-    bvisited = [[False for _ in range(width)] for _ in range(high)]
-    for y in range(high):
+    rvisited = [[False for _ in range(width)] for _ in range(height)]
+    bvisited = [[False for _ in range(width)] for _ in range(height)]
+    for y in range(height):
         for x in range(width):
             if maze[y][x] == 1:
                 # x, y좌표
                 r_loc = [x, y]
+                rvisited[y][x] = True
             elif maze[y][x] == 2:
                 b_loc = [x, y]
+                bvisited[y][x] = True
             elif maze[y][x] == 3:
                 r_arrive = [x, y]
             elif maze[y][x] == 4:
                 b_arrive = [x, y]
-    dfs(maze, 0, r_loc, b_loc, r_arrive, b_arrive, width, high, rvisited, bvisited)
-    return min(turn_arr)
+                
+    
+    dfs(maze, 0, r_loc, b_loc, r_arrive, b_arrive, width, height, rvisited, bvisited)
+    if answer == int(1e9):
+        return 0
+    else:
+        return answer
