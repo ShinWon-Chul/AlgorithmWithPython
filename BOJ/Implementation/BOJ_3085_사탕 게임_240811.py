d3c80f1868b578d6aae26900caaead6f707c6eee
shinwonchul <wcshin1201@gmail.com>
2024-08-11 13:44:02 +0900

    BOJ_3085_사탕 게임_240811.py

---
+++ BOJ/Implementation/BOJ_3085_사탕 게임_240811.py
@@ -0,0 +1,71 @@
+import copy
+from collections import deque
+N = int(input())
+graph = [ list(input()) for _ in range(N)]
+# graph = [
+#     ['Y', 'C', 'P', 'Z', 'Y'],
+#     ['C', 'Y', 'Z', 'Z', 'P'],
+#     ['C', 'C', 'P', 'P', 'P'],
+#     ['Y', 'C', 'Y', 'Z', 'C'],
+#     ['C', 'P', 'P', 'Z', 'Z']
+#  ]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+rldx = [1, -1]
+rldy = [0, 0]
+ud_dx = [0, 0]
+ud_dy = [1, -1]
+
+def bfs(graph, x, y, color, dx, dy):
+
+    queue = deque()
+    queue.append((x, y))
+    visited = [[False] * N for _ in range(N)]
+    visited[y][x] = True
+    
+    count = 1
+    while queue:
+        x, y = queue.popleft()
+        for i in range(2):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # print(visited)
+            if 0 <= nx < N and 0 <= ny < N and not visited[ny][nx]:
+                if graph[ny][nx] == color:
+                    # print(color)
+                    count += 1
+                    queue.append((nx, ny))
+
+                    visited[ny][nx] = True
+    return count
+
+res = 0
+for y in range(N):
+    for x in range(N):
+        # 오른쪽과 아래쪽만 변경
+        if x+1 < N:
+            n_graph = copy.deepcopy(graph)
+            n_graph[y][x+1], n_graph[y][x] = graph[y][x], graph[y][x+1]
+            count = bfs(n_graph, x, y, n_graph[y][x], rldx, rldy)
+            res = max(res, count)
+            count = bfs(n_graph, x, y, n_graph[y][x], ud_dx, ud_dy)
+            res = max(res, count)
+            count = bfs(n_graph, x+1, y, n_graph[y][x+1], rldx, rldy)
+            res = max(res, count)
+            count = bfs(n_graph, x+1, y, n_graph[y][x+1], ud_dx, ud_dy)
+            res = max(res, count)
+
+        if y+1 < N:
+            n_graph = copy.deepcopy(graph)
+            n_graph[y+1][x], n_graph[y][x] = graph[y][x], graph[y+1][x]
+            count = bfs(n_graph, x, y, n_graph[y][x], rldx, rldy)
+            res = max(res, count)
+            count = bfs(n_graph, x, y, n_graph[y][x], ud_dx, ud_dy)
+            res = max(res, count)
+            count = bfs(n_graph, x, y+1, n_graph[y+1][x], rldx, rldy)
+            res = max(res, count)
+            count = bfs(n_graph, x, y+1, n_graph[y+1][x], ud_dx, ud_dy)
+            res = max(res, count)
+print(res) 
