2bc70f03a88b7ccd2c666a77b34f7acf58ba2e35
shinwonchul <wcshin1201@gmail.com>
2024-08-12 18:52:05 +0900

    BOJ_1922_네트워크 연결_240812(MST간선합 구하기).py

---
+++ BOJ/Minimum_Spanning_Tree/BOJ_1922_네트워크 연결_240812(MST간선합 구하기).py
@@ -0,0 +1,39 @@
+# Find the root node of x recursively.
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# Union the two sets.
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v = int(input())
+e = int(input())
+parent = [0] * (v + 1)
+for i in range(1, v + 1):
+    parent[i] = i
+
+edges = []
+result = 0
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+# Check in order of low-cost edges.
+for edge in edges:
+    cost, a, b = edge
+    # If there will be no cycle, union.
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
