3a93b0624f25277f543093b7c2fa29de5c9e28fc
shinwonchul <wcshin1201@gmail.com>
2024-09-22 17:50:13 +0900

    BOJ_14719_빗물_240922.py

--- BOJ/Implementation/BOJ_14719_빗물_240922.py
+++ BOJ/Implementation/BOJ_14719_빗물_240922.py
@@ -1,20 +1,26 @@
-N = int(input())
-data = [list(map(int, input().split())) for _ in range(N)]
-data.sort()
-answer = 0
+H, W = list(map(int, input().split()))
+hight = list(map(int, input().split()))
 
-start, end = data[0][0], data[0][1]
+water = 0
+# 물의 높이는 1부터 시작
+for h in range(1, H+1):
+    wa = 0
+    # 가장 왼쪽은 물이 찰 수 없음
+    for w in range(1, W):
 
-for i in range(1, N):
-    s, e = data[i]
-    
-    if end < s:  # 이전 구간과 겹치지 않으면
-        answer += abs(end - start)  # 누적된 구간 길이를 계산
-        start, end = s, e  # 새로운 구간으로 초기화
-    else:
-        end = max(end, e)  # 겹치는 구간을 합침
+        # 물이 처음 차기 시작함
+        # 이전 위치의 블록이 현재 위치보다 크거나 같고, 현재 위치의 블록이 현재 높이보다 작다면 물이 차기 시작할 수 있음
+        if hight[w-1] >= h and hight[w] < h:
+            wa += 1
 
-# 마지막 구간 계산
-answer += abs(end - start)
+        # 물을 연속으로 채움
+        # 물이 차기 시작했고, 현재 위치의 블록이 물의 높이보다 작다면 물이 더 추가될 수 있음
+        elif wa > 0 and hight[w] < h:
+            wa += 1
 
-print(answer)
+        # 현재까지 쌓인 물을 더하고 초기화
+        # 현재 위치의 블록이 물의 높이보다 크거나 같다면 여태까지 쌓인 물을 더하고 물을 초기화
+        elif hight[w] >= h:
+            water += wa
+            wa = 0
+print(water)
